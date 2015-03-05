475b31f297b889ff76b5d2001eba051de4145c8d
cubano1985 <pawel.solowczuk@gmail.com>
2015-03-05 12:30:06 +0100

    Deleted mvc4, added mvc5 project, added dice model

---
+++ Model/DefensiveDiceFace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DefensiveDiceFace
+    {
+        public int Face { get; set; }
+        public float Probability { get; set; }
+        public int Armor { get; set; }
+        public int Evade { get; set; }
+        public int Dodge { get; set; }
+
+        public DefensiveDiceFace() { }
+
+        /// <summary>
+        /// Constructor for defensive dice
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="armor"></param>
+        /// <param name="evade"></param>
+        /// <param name="dodge">0 for no dodge, 1 for dodge</param>
+        public DefensiveDiceFace(int face, int armor, int evade, int dodge)
+        {
+            this.Face = face;
+            this.Armor = armor;
+            this.Evade = evade;
+            this.Dodge = dodge;
+            this.Probability = 1 / 6;
+        }
+    }
+}
---
+++ Model/OffensiveDiceFace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class OffensiveDiceFace
+    {
+        public int Face { get; set; }
+        public float Probability { get; set; }
+        public int Range { get; set; }
+        public int Damage { get; set; }
+        public int Surge { get; set; }
+
+        public OffensiveDiceFace() { }
+
+        /// <summary>
+        /// Offensive for offensive dice
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="range"></param>
+        /// <param name="damage"></param>
+        /// <param name="surge"></param>
+        public OffensiveDiceFace(int face, int range, int damage, int surge)
+        {
+            this.Face = face;
+            this.Range = range;
+            this.Damage = damage;
+            this.Surge = surge;
+            this.Probability = 1 / 6;
+        }
+    }
+}
